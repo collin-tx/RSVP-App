e24fe914c79abe37aefcd39dfb0f36326887324b
Collin Miller Smith <44204853+collin-tx@users.noreply.github.com>
2019-01-31 22:08:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+document.addEventListener('D);
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 
@@ -19,9 +20,38 @@ filterCheckBox.addEventListener('change', (e) => {
   if(isChecked) {
     for (let i = 0; i < lis.length; i += 1){
       let li= lis[i];
+      li.style.display = '';
     }
   }
+});
+
+function createLI(text) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);
+  const label - document.createElement('label');
+  label.textContent = 'Confirmed';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.appendChild(checkbox);
+  li.appendChild(label);
+  const editButton = document.createElement('button');
+  editButton.textContent = 'edit';
+  li.appendChild(editButton);
+  const removeButton.textContent = 'remove';
+  li.appendChild(removeButton);
+  return li; 
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = '';
+  const li = createLI(text);
+  ul.appendChild(li);
+})
+
 
   
   
